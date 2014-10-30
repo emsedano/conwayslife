9b320ce0687818eeafc61e19255f2016ebcc2226
Elias Martinez <emsedano@live.com.mx>
2014-10-30 16:19:31 -0600

    Cell object

---
+++ js/cell.js
@@ -0,0 +1,38 @@
+
+
+function Cell(x, y, populated){
+	this.px = x;
+	this.py = y;
+	if(populated)
+		this.populated = populated;
+	else
+		this.populated = false;
+}
+
+Cell.prototype.setPopulate = function (val){
+	this.populated = val;
+}
+
+Cell.prototype.isPopulated = function(){
+	return this.populated;
+};
+
+
+Can a freelancer place a bid outside the budget limits set by an employer?
+
+Can a freelancer retake a paid exam?
+
+What is the minimum amount that you can withdraw via Freelancer Debit Card?
+
+What can be done if an Employer does not provide a needed file for the project?
+
+Can a Freelancer change his username?
+
+What is the minimum number of reviews required for a freelancer to withdraw money by Express Withdrawal?
+
+Freelancers who take an exam are more likely to be awarded by:
+
+How should communications, information and file exchanges performed?
+
+How many XP Bonus for connecting with Facebook ?
+
