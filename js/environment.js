7e201e1208d8f61c70622ea6c87ccfeb388f5f31
Elias Martinez <elias.martinez@amplemind.com>
2014-11-05 23:08:31 -0600

    Fix issues

--- js/environment.js
+++ js/environment.js
@@ -1,122 +1,135 @@
+//================================= ENVIRONMENT =================================
 
-	
-	var ConwaysLifeGame = {
+function Environment(w, h, target) {
+    this.target = $(target);
+    this.w = w;
+    this.h = h;
 
-		environment: null,
-		daysGoBy: null,
-		running: false,
-		lifeCycles: 0,
+    this.fields = new Array(w);
 
-		setUp: function() {
-			ConwaysLifeGame.environment = new Environment(50,50, "#environment");
-			ConwaysLifeGame.environment.init();
-		},
+    for (var i = 0; i < w; i++) {
+        //define cada elemento como una matriz de longitud w
+        this.fields[i] = new Array(h);
+        for (var j = 0; j < h; j++) {
+            /* asigna a cada elemento de la matriz bidimensional 
+             * los valores de i y j */
+            this.fields[i][j] = new Cell(i, j);
+        }
+    }
+}
 
-		start: function(){
-			ConwaysLifeGame.running = true;
-			if (ConwaysLifeGame.daysGoBy === null)
-				ConwaysLifeGame.daysGoBy = setInterval(function(){
-					ConwaysLifeGame.executeLifeCicle();
-				}, 500);
-		},
+Environment.prototype.NORTH = 'N';
+Environment.prototype.NOREAST = 'NE';
+Environment.prototype.NORWEST = 'NW';
+Environment.prototype.SOUTH = 'S';
+Environment.prototype.SOUTHEAST = 'SE';
+Environment.prototype.SOUTHWEST = 'SW';
+Environment.prototype.EAST = 'E';
+Environment.prototype.WEST = 'W';
 
-		pause: function(){
-			ConwaysLifeGame.running = false;
-		},
+Environment.prototype.init = function () {
 
-		stop: function(){
-			clearInterval(ConwaysLifeGame.daysGoBy);
-			ConwaysLifeGame.pause();
-			ConwaysLifeGame.daysGoBy = null;
-		},		
+    var tr = "";
+    var td = "";
+    var css = "";
+    for (i = 0; i < this.w; i++) {
 
-		isNotZeroGeneration: function(){
-			if(ConwaysLifeGame.lifeCycles > 0)
-				return true;
-			else 
-				return false;
-		},
+        tr = "<tr id='row_" + i + "'>";
+        td = "";
 
-		executeLifeCicle: function(){
+        for (j = 0; j < this.h; j++) {
+            css = "";
+            if (this.getCell(i, j) !== null && this.getCell(i, j).isPopulated()) css = "alive";
+            td += "<td class='" + css + "' ondblclick='ConwaysLifeGame.togglePopulated(" + i + "," + j + ");'></td>";
 
-			if( ConwaysLifeGame.running){
+        }
 
-				if (ConwaysLifeGame.isNotZeroGeneration() )
-					ConwaysLifeGame.prepareEnvironment();
+        tr += td;
+        tr += "</tr>";
 
+        this.target.append(tr);
+    }
+};
 
-				var _env = ConwaysLifeGame.environment;
-				var cell = null;
+Environment.prototype.refreshEnvironment = function () {
+    this.target.html("");
+    this.init();
+};
 
-				var i, j;
-				for( i=0; i < _env.fields.length; i++){
-					for( j=0; j < _env.fields[i].length; j++){
-						
-						cell = _env.getCell(i, j);
-						
-						if(cell.isPopulated()){
-							ConwaysLifeGame.evaluatePopulatedCell(cell);
-						}else{
-							ConwaysLifeGame.evaluateUnpopulatedCell(cell);
-						}	
-					}
-				}
+Environment.prototype.setCell = function (Cell) {
+    if (this.fields[Cell.px][Cell.py] === null) this.fields[Cell.px][Cell.py] = Cell;
+};
 
-				_env.refreshEnvironment();
+Environment.prototype.getCell = function (x, y) {
+    return this.fields[x][y];
+};
 
-			}
+Environment.prototype.hasNeighborAt = function (Cell, DIR) {
 
-			ConwaysLifeGame.lifeCycles++;
-		}, 
+    var neighborgCell = this.doesCellExist(Cell, DIR);
 
-		prepareEnvironment: function(){
-			var _env = ConwaysLifeGame.environment;
-			var cell = null;
-			var i, j;
-			for( i=0; i < _env.fields.length; i++){
-				for( j=0; j < _env.fields[i].length; j++){
-					
-					cell = _env.getCell(i, j);
+    if (neighborgCell) return neighborgCell.isPopulated();
 
-					if(cell.isGoner)
-						cell.setPopulate(false);
-					else if (cell.isPregned)
-						cell.setPopulate(true);
-						
-				}
-			}
-		},
+    return false;
 
-		evaluatePopulatedCell: function(cell){
+};
 
+Environment.prototype.doesCellExist = function (Cell, DIR) {
 
+    if (Cell) {
 
-			var neighbors = ConwaysLifeGame.environment.countNeighbors(cell);
-   		    
-    	if(neighbors <= 1){// Each cell with one or no neighbors dies, as if by loneliness.
-				cell.willDie();
-			}else if(neighbors >= 4){
-				// Each cell with four or more neighbors dies, as if by overpopulation.
-				cell.willDie(false);
-			} 
-			// else Each cell with two or three neighbors survives.
-		},
+        if (DIR === this.NORTH && Cell.px > 0) {
+            //NORTH
+            return this.getCell(Cell.px - 1, Cell.py);
 
-		evaluateUnpopulatedCell: function(cell){
-			// Each cell with three neighbors becomes populated.																						   		   
-    	if(ConwaysLifeGame.environment.countNeighbors(cell) == 3){// Each cell with one or no neighbors dies, as if by loneliness.
-				cell.fertilize();
-			}
-		},
+        } else if (DIR === this.SOUTH && Cell.px < this.h - 1) {
+            //SOUTH
+            return this.getCell(Cell.px + 1, Cell.py);
 
-		togglePopulated: function(x, y){
-			if (ConwaysLifeGame.environment.getCell(x, y).isPopulated())
-				ConwaysLifeGame.environment.getCell(x, y).setPopulate(false);
-			else
-				ConwaysLifeGame.environment.getCell(x, y).setPopulate(true);
+        } else if (DIR === this.EAST && Cell.py < this.w - 1) {
+            //EAST
+            return this.getCell(Cell.px, Cell.py + 1);
 
-			ConwaysLifeGame.environment.refreshEnvironment();
-		}
-	};
+        } else if (DIR === this.WEST && Cell.py > 0) { /// West default
+            //WEST
+            return this.getCell(Cell.px, Cell.py - 1);
+
+        } else if (DIR === this.NOREAST) {
+            //NOREAST
+            return this.doesCellExist(this.doesCellExist(Cell, this.NORTH), this.EAST);
+
+        } else if (DIR === this.NORWEST) {
+            //NORWEST
+            return this.doesCellExist(this.doesCellExist(Cell, this.NORTH), this.WEST);
+
+        } else if (DIR === this.SOUTHEAST) {
+            //SOUTHEAST
+            return this.doesCellExist(this.doesCellExist(Cell, this.SOUTH), this.EAST);
+
+        } else if (DIR === this.SOUTHWEST) {
+            //SOUTHWEST
+            return this.doesCellExist(this.doesCellExist(Cell, this.SOUTH), this.WEST);
+        } else {
+            return false;
+        }
+
+    } else {
+        return false;
+    }
 
 
+};
+
+Environment.prototype.countNeighbors = function (Cell) {
+    var neighborgs = 0;
+    if (this.hasNeighborAt(Cell, this.NORTH)) neighborgs++;
+    if (this.hasNeighborAt(Cell, this.NOREAST)) neighborgs++;
+    if (this.hasNeighborAt(Cell, this.NORWEST)) neighborgs++;
+    if (this.hasNeighborAt(Cell, this.SOUTH)) neighborgs++;
+    if (this.hasNeighborAt(Cell, this.SOUTHEAST)) neighborgs++;
+    if (this.hasNeighborAt(Cell, this.SOUTHWEST)) neighborgs++;
+    if (this.hasNeighborAt(Cell, this.EAST)) neighborgs++;
+    if (this.hasNeighborAt(Cell, this.WEST)) neighborgs++;
+
+    return neighborgs;
+};
--- js/main.js
+++ js/main.js
@@ -1,87 +1,110 @@
+//================= Conways life object ========================
 
-	
-	var ConwaysLifeGame = {
+var ConwaysLifeGame = {
 
-		environment: null,
-		daysGoBy: null,
-		running: false,
+    environment: null,
+    daysGoBy: null,
+    running: false,
+    lifeCycles: 0,
 
-		setUp: function() {
-			ConwaysLifeGame.environment = new Environment(50,50, "#environment");
-			ConwaysLifeGame.environment.init();
-		},
+    setUp: function () {
+        ConwaysLifeGame.environment = new Environment(50, 50, "#environment");
+        ConwaysLifeGame.environment.init();
+    },
 
-		start: function(){
-			ConwaysLifeGame.running = true;
-			if (ConwaysLifeGame.daysGoBy === null)
-				ConwaysLifeGame.daysGoBy = setInterval(function(){
-					ConwaysLifeGame.executeLifeCicle();
-				}, 600);
-		},
+    start: function () {
+        ConwaysLifeGame.running = true;
+        if (ConwaysLifeGame.daysGoBy === null) ConwaysLifeGame.daysGoBy = setInterval(function () {
+            ConwaysLifeGame.executeLifeCicle();
+        }, 500);
+    },
 
-		pause: function(){
-			ConwaysLifeGame.running = false;
-		},
+    pause: function () {
+        ConwaysLifeGame.running = false;
+    },
 
-		stop: function(){
-			clearInterval(ConwaysLifeGame.daysGoBy);
-			ConwaysLifeGame.pause();
-			ConwaysLifeGame.daysGoBy = null;
-		},		
+    stop: function () {
+        clearInterval(ConwaysLifeGame.daysGoBy);
+        ConwaysLifeGame.pause();
+        ConwaysLifeGame.daysGoBy = null;
+    },
 
-		executeLifeCicle: function(){
-			if( ConwaysLifeGame.running){
+    isNotZeroGeneration: function () {
+        if (ConwaysLifeGame.lifeCycles > 0) return true;
+        else return false;
+    },
 
+    executeLifeCicle: function () {
 
-				var _env = ConwaysLifeGame.environment;
-				var cell = null;
+        if (ConwaysLifeGame.running) {
 
-				var i, j;
-				for( i=0; i < _env.fields.length; i++){
-					for( j=0; j < _env.fields[i].length; j++){
-						
-						cell = _env.getCell(i, j);
-						
-						if(cell.isPopulated()){
-							ConwaysLifeGame.evaluatePopulatedCell(cell);
-						}else{
-							ConwaysLifeGame.evaluateUnpopulatedCell(cell);
-						}	
-					}
-				}
-
-				_env.refreshEnvironment();
-
-			}
-
-		}, 
-
-		evaluatePopulatedCell: function(cell){
-			var neighbors = ConwaysLifeGame.environment.countNeighbors(cell);
-   		    
-    	if(neighbors <= 1){// Each cell with one or no neighbors dies, as if by loneliness.
-				cell.setPopulate(false);
-			}else if(neighbors >= 4){
-				// Each cell with four or more neighbors dies, as if by overpopulation.
-				cell.setPopulate(false);
-			} 
-			// else Each cell with two or three neighbors survives.
-		},
-
-		evaluateUnpopulatedCell: function(cell){
-			// Each cell with three neighbors becomes populated.																						   		   
-    	if(ConwaysLifeGame.environment.countNeighbors(cell) == 3){// Each cell with one or no neighbors dies, as if by loneliness.
-				cell.setPopulate(true);
-			}
-		},
-
-		togglePopulated: function(x, y){
-			if (ConwaysLifeGame.environment.getCell(x, y).isPopulated())
-				ConwaysLifeGame.environment.getCell(x, y).setPopulate(false);
-			else
-				ConwaysLifeGame.environment.getCell(x, y).setPopulate(true);
-			ConwaysLifeGame.environment.refreshEnvironment();
-		}
-	};
+            if (ConwaysLifeGame.isNotZeroGeneration()) ConwaysLifeGame.prepareEnvironment();
 
 
+            var _env = ConwaysLifeGame.environment;
+            var cell = null;
+
+            var i, j;
+            for (i = 0; i < _env.fields.length; i++) {
+                for (j = 0; j < _env.fields[i].length; j++) {
+
+                    cell = _env.getCell(i, j);
+
+                    if (cell.isPopulated()) {
+                        ConwaysLifeGame.evaluatePopulatedCell(cell);
+                    } else {
+                        ConwaysLifeGame.evaluateUnpopulatedCell(cell);
+                    }
+                }
+            }
+
+            _env.refreshEnvironment();
+
+        }
+
+        ConwaysLifeGame.lifeCycles++;
+    },
+
+    prepareEnvironment: function () {
+        var _env = ConwaysLifeGame.environment;
+        var cell = null;
+        var i, j;
+        for (i = 0; i < _env.fields.length; i++) {
+            for (j = 0; j < _env.fields[i].length; j++) {
+
+                cell = _env.getCell(i, j);
+
+                if (cell.isGoner) cell.setPopulate(false);
+                else if (cell.isPregned) cell.setPopulate(true);
+
+            }
+        }
+    },
+
+    evaluatePopulatedCell: function (cell) {
+
+        var neighbors = ConwaysLifeGame.environment.countNeighbors(cell);
+
+        if (neighbors <= 1) { // Each cell with one or no neighbors dies, as if by loneliness.
+            cell.willDie();
+        } else if (neighbors >= 4) {
+            // Each cell with four or more neighbors dies, as if by overpopulation.
+            cell.willDie(false);
+        }
+        // else Each cell with two or three neighbors survives.
+    },
+
+    evaluateUnpopulatedCell: function (cell) {
+        // Each cell with three neighbors becomes populated.                                                                                                   
+        if (ConwaysLifeGame.environment.countNeighbors(cell) == 3) { // Each cell with one or no neighbors dies, as if by loneliness.
+            cell.fertilize();
+        }
+    },
+
+    togglePopulated: function (x, y) {
+        if (ConwaysLifeGame.environment.getCell(x, y).isPopulated()) ConwaysLifeGame.environment.getCell(x, y).setPopulate(false);
+        else ConwaysLifeGame.environment.getCell(x, y).setPopulate(true);
+
+        ConwaysLifeGame.environment.refreshEnvironment();
+    }
+};
